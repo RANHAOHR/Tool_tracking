e35def252f5886aeb34fc91d86603933b5e6d743
RANHAOHR <rxh349@case.edu>
2016-10-25 18:34:03 -0400

    model class

--- tooltrack/include/tool_tracking_lib/tool_model.h
+++ tooltrack/include/tool_tracking_lib/tool_model.h
@@ -11,19 +11,51 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
+
+
 class ToolModel{
  private:
 
-        //basic rotaions and traslations
-		cv::Matx<double,3,1> tvec;	//translation vector
-		cv::Matx<double,3,1> rvec;	//rotation vector
+		struct toolModel {
+			cv::Matx<double,3,1> tvec;	//translation vector
+			cv::Matx<double,3,1> rvec;	//rotation vector
+			double theta_ellipse;
+			double theta_grip_1;
+			double theta_grip_2;  //9 DOF
 
+			cv::Matx<double,3,1> w_z;   //for computational purpose
+		    cv::Matx<double,3,1> w_x;
+
+			//built in operator that handles copying the toolModel in efficient way
+			toolModel& operator =(const toolModel src){
+				this->theta_ellipse = src.theta_ellipse;
+				this->theta_grip_1 = src.theta_grip_1;
+				this->theta_grip_2 = src.theta_grip_2;
+				for(int i(0); i<3; i++){
+					this->tvec(i) = src.tvec(i);
+					this->rvec(i) = src.rvec(i);
+					this->w_z(i) = src.w_z(i);
+					this->w_x(i) = src.w_x(i);
+				}
+				return *this;
+			}
+
+			// toolModel constructor, creates an empty toolModel
+			toolModel(void){
+				this->theta_ellipse = 0.0;
+				this->theta_grip_1 = 0.0;
+				this->theta_grip_2 = 0.0;
+				for(int i(0); i<3; i++){
+					this->tvec(i) = 0.0;
+					this->rvec(i) = 0.0;
+					this->w_z(i) = 0.0;
+					this->w_x(i) = 0.0; 
+				}
+			}
+
+		};   //end struct
+        
 		cv::Matx<double,3,3> I;
-
-
-		double theta_ellipse;
-		double theta_grip_1;
-		double theta_grip_2;
 
 		std::vector< glm::vec3 > body_vertices;
 		std::vector< glm::vec3 > ellipse_vertices;
@@ -36,15 +68,11 @@ class ToolModel{
 
  		 void load_model_vertices();
 
+ 		toolModel setRandomConfig(const toolModel &initial, double stdev, double mean);
 
+ 		// cv::Rect renderTool();
 
- 		void setZeroConfig();
-
- 		void setRandomConfig();
-
- 		cv::Rect renderTool(cv::Mat &, const needleGeometry &, const cv::Mat &, int=1, cv::OutputArray = cv::noArray(), cv::OutputArray = cv::noArray());
-
- 		double calculateMatchingScore(cv::Mat &, const cv::Mat &, cv::Rect &, bool = false);
+ 		// double calculateMatchingScore();
 
 };
 
--- tooltrack/src/tool_model.cpp
+++ tooltrack/src/tool_model.cpp
@@ -9,6 +9,7 @@
 #include <cwru_opencv_common/projective_geometry.h>
 
 #include <tool_tracking_lib/tool_model.h>
+#include <math.h>
 
 using cv_projective::reprojectPoint;
 boost::mt19937 rng(time(0));
@@ -16,15 +17,7 @@ boost::mt19937 rng(time(0));
 //constructor
 ToolModel::ToolModel(){
 
-	for(int i(0); i<3; i++){
-	tvec(i) = 0.0;
-	rvec(i) = 0.0; 
-	}
-
-	theta_ellipse = 0.0;
-	theta_grip_1 = 0.0;
-	theta_grip_2 = 0.0;
-
+////////
 	for(int i(0); i<3; i++){
 		for(int j(0);j<3;j++){
 			I(i)(j) = 0.0;
@@ -33,7 +26,7 @@ ToolModel::ToolModel(){
 	I(1)(1) = 1;
 	I(2)(2) = 1;
 	I(3)(3) = 1;
-
+///////
 };
 
 double ToolModel::randomNumber(double stdev, double mean){
@@ -50,51 +43,124 @@ ToolModel::load_model_vertices(){
 
 };
 
+toolModel ToolModel::setRandomConfig(const toolModel &initial, double stdev, double mean)
+{
+	// copy initial toolModel
+	toolModel newTool = initial;
 
-ToolModel::setZeroConfig(){
-	//std::vector< std::vector<double> > exp_R;
-	//exp_R.resize(3);
+	//create normally distributed random number with the given stdev and mean
+
+	//TODO: pertub all translation components
+	newTool.tvec(0) += randomNumber(stdev,mean); 
+	newTool.tvec(1) += randomNumber(stdev,mean);
+	newTool.tvec(2) += randomNumber(stdev,mean);
+	
+	double angle = randomNumber(stdev,mean);
+	newTool.rvec(0) += (angle/10.0)*1000.0; //rotation on x axis +/-5 degrees
+
+	angle = randomNumber(stdev,mean);
+	newTool.rvec(0) += (angle/10.0)*1000.0; ////rotation on x axis +/-5 degrees
+
+	angle = randomNumber(stdev,mean);
+	newTool.rvec(2) += (angle/10.0)*1000.0; //rotation on z axis +/-5 degrees
 
 
+    /**************smaple the angles of the joints**************/
+	//-90,90//
+	angle = randomNumber(stdev,mean);
+	newTool.theta_ellipse += (angle/10.0)*1000.0;
+	if (newTool.theta_ellipse < -M_PI/2 || newTool.theta_ellipse > M_PI/2)   //use M_PI HERE
+		newTool.theta_ellipse = randomNumber(stdev,mean);
+
+	// lets a assign the upside one 1, and set positive as clockwise 
+	angle = randomNumber(stdev,mean);
+	newTool.theta_grip_1 += (angle/10.0)*1000.0;
+	if (newTool.theta_grip_1 < -1.2*M_PI/2 || newTool.theta_grip_1 > 1.2*M_PI/2)   //use M_PI HERE
+		newTool.theta_grip_1 = randomNumber(stdev,mean);
+	
+	// lets a assign the udownside one 2, and set positive as clockwise
+	angle = randomNumber(stdev,mean);
+	newTool.theta_grip_2 += (angle/10.0)*1000.0;
+	if (newTool.theta_grip_1 < -1.2*M_PI/2 || newTool.theta_grip_1 > 1.2*M_PI/2)   //use M_PI HERE
+		newTool.theta_grip_1 = randomNumber(stdev,mean);
+
+	///if the two joints get overflow/////
+	if (newTool.theta_grip_1 > newTool.theta_grip_2)
+		newTool.theta_grip_1 = newTool.theta_grip_2 - randomNumber(stdev,mean);
+
+
+
+   /***********compute exponential map for forward kinematics**********/
 	cv::Matx<double,3,3> roll_mat;
 	cv::Matx<double,3,3> pitch_mat;
 	cv::Matx<double,3,3> yaw_mat;
 
-    roll_mat(0)(0) = 0;
-    roll_mat(0)(1) = -rvec(3);
-    roll_mat(0)(2) = rvec(2);
-    roll_mat(1)(0) = rvec(3);
-    rvec_mat(1)(1) = 0;
-    rvec_mat(1)(2) = -rvec(1);
-    rvec_mat(2)(0) = -rvec(2);
-    rvec_mat(2)(1) = rvec(1);
-    rvec_mat(2)(2) = 0;	
+    roll_mat(0)(0) = 1;
+    roll_mat(0)(1) = 0;
+    roll_mat(0)(2) = 0;
+    roll_mat(1)(0) = 0;
+    roll_mat(1)(1) = cos(newTool.rvec(0));
+    roll_mat(1)(2) = -sin(newTool.rvec(0));
+    roll_mat(2)(0) = 0;
+    roll_mat(2)(1) = sin(newTool.rvec(0));
+    roll_mat(2)(2) = cos(newTool.rvec(0));
 
+    pitch_mat(0)(0) = cos(newTool.rvec(1));
+    pitch_mat(0)(1) = 0;
+    pitch_mat(0)(2) = sin(newTool.rvec(1));
+    pitch_mat(1)(0) = 0;
+    pitch_mat(1)(1) = 1;
+    pitch_mat(1)(2) = 0;
+    pitch_mat(2)(0) = sin(newTool.rvec(1));
+    pitch_mat(2)(1) = 0;
+    pitch_mat(2)(2) = cos(newTool.rvec(1));
+
+    yaw_mat(0)(0) = cos(newTool.rvec(2));
+    yaw_mat(0)(1) = -sin(newTool.rvec(2));
+    yaw_mat(0)(2) = 0;
+    yaw_mat(1)(0) = sin(newTool.rvec(2));
+    yaw_mat(1)(1) = cos(newTool.rvec(2));
+    yaw_mat(1)(2) = 0;
+    yaw_mat(2)(0) = 0;
+    yaw_mat(2)(1) = 0;
+    yaw_mat(2)(2) = 1;
+
+    cv::Matx<double,3,3> rotation_mat;
+    rotation_mat = yaw_mat * pitch_mat * roll_mat;
     
-    //w is rvec
-    rvec_mat(0)(0) = 0;
-    rvec_mat(0)(1) = -rvec(3);
-    rvec_mat(0)(2) = rvec(2);
-    rvec_mat(1)(0) = rvec(3);
-    rvec_mat(1)(1) = 0;
-    rvec_mat(1)(2) = -rvec(1);
-    rvec_mat(2)(0) = -rvec(2);
-    rvec_mat(2)(1) = rvec(1);
-    rvec_mat(2)(2) = 0;
+    //w is z of rotation mat
+    
+    newTool.w_z(0) = rotation_mat(0)(2);
+    newTool.w_z(1) = rotation_mat(1)(2);
+    newTool.w_z(2) = rotation_mat(2)(2);
+
+    newTool.w_x(0) = rotation_mat(0)(0);
+    newTool.w_x(1) = rotation_mat(1)(0);
+    newTool.w_x(2) = rotation_mat(2)(0);
+    /*get new tool model info*/
+
+	return newTool;
+}
+
+
+ToolModel::setRandomConfig(double stdev, double mean){
+
+	tvec(0) += randomNumber(stdev,mean);
+
+    cv::Matx<double,3,3> w_mat;
+
+	w_mat(0)(0) = 0;
+    w_mat(0)(1) = -w_z(2);
+    w_mat(0)(2) = w_z(1);
+    w_mat(1)(0) = w_z(2);
+    w_mat(1)(1) = 0;
+    w_mat(1)(2) = -w_z(0);
+    w_mat(2)(0) = -w_z(1);
+    w_mat(2)(1) = w_z(0);
+    w_mat(2)(2) = 0;
 
 	cv::Matx<double,3,3> exp_R;
-    exp_R = I + rvec_mat * sin(theta_ellipse) + rvec_mat * rvec_mat * (1-cos(theta_ellipse));
-
-
-
-};
-
-
-ToolModel::setRandomConfig(){
-
-
-
-
+    exp_R = I + w_mat * sin(theta_ellipse) + w_mat * w_mat * (1-cos(theta_ellipse));
 
 
 
