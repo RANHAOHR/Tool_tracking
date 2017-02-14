0c29e984e1d210859675c07ecddf6b01b1924b06
RANHAOHR <rxh349@case.edu>
2017-02-13 23:15:00 -0500

    change chamfer matching for old version opencv

--- tool_model/cmake-build-debug/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/ranhao/ros_ws/src/Tool_tracking/tool_model/include".split(';') if "/home/ranhao/ros_ws/src/Tool_tracking/tool_model/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "image_transport;roscpp;sensor_msgs;std_msgs;message_runtime;cwru_opencv_common".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-ltool_model_lib".split(';') if "-ltool_model_lib" != "" else []
-PROJECT_NAME = "tool_model"
-PROJECT_SPACE_DIR = "/home/ranhao/ros_ws/src/Tool_tracking/tool_model/cmake-build-debug/devel"
-PROJECT_VERSION = "0.0.0"
--- tool_model/cmake-build-debug/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/usr/local/include".split(';') if "/usr/local/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "image_transport;roscpp;sensor_msgs;std_msgs;message_runtime;cwru_opencv_common".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-ltool_model_lib".split(';') if "-ltool_model_lib" != "" else []
-PROJECT_NAME = "tool_model"
-PROJECT_SPACE_DIR = "/usr/local"
-PROJECT_VERSION = "0.0.0"
