4b826774d3301d300daee8a131065300c77d0009
RANHAOHR <rxh349@case.edu>
2017-03-07 20:42:43 -0500

    need to add shape.hpp

--- tool_model/src/tool_model_main.cpp
+++ tool_model/src/tool_model_main.cpp
@@ -15,6 +15,8 @@
 #include "std_msgs/MultiArrayLayout.h"
 #include "std_msgs/Float64MultiArray.h"
 
+#include "opencv2/shape.hpp"
+
 using namespace std;
 
 int main(int argc, char **argv) {
@@ -110,6 +112,8 @@ int main(int argc, char **argv) {
 
     cv::cvtColor(segImg, segImg, CV_BGR2GRAY); //convert it to grey scale
 
+
+    cv::compute
     cv::imshow("tool image: ",testImg );
     cv::imshow("segImg : ",segImg );
 
