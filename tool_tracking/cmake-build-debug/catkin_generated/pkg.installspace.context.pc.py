720a6c7d271af765e61356cf4447be292b1d4acf
RANHAOHR <rxh349@case.edu>
2017-08-14 13:59:02 -0400

    edit tool model and PF functions

--- tool_tracking/cmake-build-debug/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/usr/local/include".split(';') if "/usr/local/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "cv_bridge;image_transport;roscpp;sensor_msgs;std_msgs;message_runtime;cwru_opencv_common;tool_model;geometry_msgs;cwru_davinci_interface;cwru_davinci_kinematics;xform_utils".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-ltool_tracking_particle;-ltool_tracking_kalman".split(';') if "-ltool_tracking_particle;-ltool_tracking_kalman" != "" else []
-PROJECT_NAME = "tool_tracking"
-PROJECT_SPACE_DIR = "/usr/local"
-PROJECT_VERSION = "0.0.0"
