2fd0c2dc79eb4e3c8ab42d702278bfbbc1d50c13
RANHAOHR <rxh349@case.edu>
2017-08-16 16:28:43 -0400

    edit tool params

--- tool_model/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rxh349/ros_ws/src/Tool_tracking/tool_model
-# Build directory: /home/rxh349/ros_ws/src/Tool_tracking/tool_model/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
