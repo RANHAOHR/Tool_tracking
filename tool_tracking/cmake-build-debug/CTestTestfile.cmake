e96fb42d8773d10b8c61ad9fa54909a924364ec6
RANHAOHR <rxh349@case.edu>
2017-09-04 19:49:19 -0500

    add ne m noise set for pose collection

--- tool_tracking/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ranhao/ros_ws/src/Tool_tracking/tool_tracking
-# Build directory: /home/ranhao/ros_ws/src/Tool_tracking/tool_tracking/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
